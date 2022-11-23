dda4591e2c11d8afe09946e572a6e7a2f4400704
Guilherme <guilherme.cst@outlook.com.br>
2022-11-23 07:40:16 -0300

    Update files

--- models.py
+++ models.py
@@ -1,13 +1,21 @@
-from app import db
+from app import db, login_manager
+from werkzeug.security import generate_password_hash, check_password_hash
+from flask_login import UserMixin
 
 
-class Usuarios(db.Model):
+@login_manager.user_loader
+def get_user(id):
+    return Usuarios.query.filter_by(id=id).first()
+
+
+class Usuarios(db.Model, UserMixin):
+    __tablename__ = 'usuarios'
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     nome = db.Column(db.String(20), nullable=False)
     sobrenome = db.Column(db.String(50), nullable=False)
     genero = db.Column(db.String(1), nullable=False)
     data_nasc = db.Column(db.String(10), nullable=False)
-    email = db.Column(db.String(100), nullable=False)
+    email = db.Column(db.String(100), nullable=False, unique=True)
     telefone = db.Column(db.String(15), nullable=False)
     cpf = db.Column(db.String(14), nullable=False)
     cep = db.Column(db.String(9), nullable=False)
@@ -17,7 +25,7 @@ class Usuarios(db.Model):
     numero = db.Column(db.String(5), nullable=False)
     complemento = db.Column(db.String(15))
     bairro = db.Column(db.String(30), nullable=False)
-    nickname = db.Column(db.String(20), nullable=False)
+    nickname = db.Column(db.String(20), nullable=False, unique=True)
     senha = db.Column(db.String(50), nullable=False)
     agendamento = db.relationship('Agendamentos', backref='id', lazy=True)
 
@@ -26,12 +34,16 @@ class Usuarios(db.Model):
                            email=email, telefone=telefone,
                            cpf=cpf, cep=cep, uf=uf, cidade=cidade,
                            rua=rua, numero=numero, complemento=complemento,
-                           bairro=bairro, nickname=nickname, senha=senha)
+                           bairro=bairro, nickname=nickname, senha=generate_password_hash(senha))
         db.session.add(usuario)
         db.session.commit()
 
+    def verificar_senha(self, pwd):
+        return check_password_hash(self.senha, pwd)
 
-class Agendamentos(db.Model):
+
+class Agendamentos(db.Model, UserMixin):
+    __tablename__ = 'agendamentos'
     id_agendamento = db.Column(
         db.Integer, primary_key=True, autoincrement=True)
     nome_cliente = db.Column(db.String(20), nullable=False)
